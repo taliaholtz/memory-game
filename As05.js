b58b041725e0acf121164b32be0464e7ca6478da
talshir <talia.holtzman00@gmail.com>
2017-12-20 17:27:11 +0200

    added header

--- As05.js
+++ As05.js
@@ -1,7 +1,12 @@
+var showRules=function(){
+    document.getElementById("rules").style.display="block";
+    document.getElementById("close").addEventListener("click",closeRules);
+}
+var closeRules=function(){
+    document.getElementById("rules").style.display="none";
+}
 var gamePlay=function(){
-    var display=event.target.value;
     event.target.classList.toggle("cardBack");
-    event.target.classList.toggle(display);
 }
 var shuffleDeck=function(array){
     var n=array.length;
@@ -14,22 +19,45 @@ var shuffleDeck=function(array){
     return array;
 }
 var setUp=function(){
+    var board=document.getElementById("gameboard");
+    while(board.hasChildNodes()){
+        board.removeChild(board.firstChild);
+    }
     for(var i=0;i<2;i++){
         shuffleDeck(cardBank);
         for(var j=0;j<cardBank.length;j++){
             var card=document.createElement('div');            
-            card.className="cardBack card";
-            card.value=cardBank[j];
+            card.className="cardBack card "+cardBank[j];
             card.addEventListener("click",gamePlay);
             document.getElementById("gameboard").appendChild(card);    
         }
     }
 }
 var endGame=function(){
-    document.getElementById("myModal").style.display="block";
+    document.getElementById("winner").style.display="block";
 }
 
 var cardBank=["shark1","shark2","shark3","shark4","shark5","shark6"];
+
+var header=document.createElement('div');
+header.id="header";
+document.body.appendChild(header);
+var nav=document.createElement('div');
+nav.id="nav";
+header.appendChild(nav);
+var menu=["New Game","Rules"];
+for (var i=0;i<menu.length;i++){
+    var link=document.createElement('a');
+    link.id=menu[i];
+    link.class="header";
+    link.textContent=menu[i];
+    nav.appendChild(link);
+}
+document.getElementById("New Game").addEventListener("click",setUp);
+document.getElementById("Rules").addEventListener("click",showRules);
+
+var pageBreak=document.createElement('hr');
+document.body.appendChild(pageBreak);
 
 var game=document.createElement('div');
 game.id="gameboard";
